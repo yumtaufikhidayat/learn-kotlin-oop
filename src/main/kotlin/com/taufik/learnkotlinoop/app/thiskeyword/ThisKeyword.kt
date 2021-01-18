73cb84eb5ff9b7bf40d4f0b8acd38d79fcfc147e
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-01-18 08:09:25 +0700

    Adding this keyword.

---
+++ src/main/kotlin/com/taufik/learnkotlinoop/app/thiskeyword/ThisKeyword.kt
@@ -0,0 +1,11 @@
+package com.taufik.learnkotlinoop.app.thiskeyword
+
+import com.taufik.learnkotlinoop.classes.Person
+
+fun main() {
+
+    val taufik = Person()
+    taufik.firstName = "Taufik"
+
+    taufik.sayHello("Budi", "Handoko")
+}
--- src/main/kotlin/com/taufik/learnkotlinoop/classes/Person.kt
+++ src/main/kotlin/com/taufik/learnkotlinoop/classes/Person.kt
@@ -8,23 +8,25 @@ class Person {
     var lastName: String = ""
 
     // Functions inside class
+    // Also this function using "this" keyword to refer the properties
     fun sayHello(name: String) {
-        println("Hello $name, my name is $firstName")
+        println("Hello $name, my name is ${this.firstName}")
     }
 
     fun run() {
         println("I'm running")
     }
 
+    // This function using "this" keyword to refer the properties
     fun getFullName(): String {
-        return "$firstName $middleName $lastName"
+        return "${this.firstName} ${this.middleName} ${this.lastName}"
     }
 
     // Function with same name called function overloading.
     // Function overloading is a function that has same name with other function,
     // But the parameter, a number of parameter, or data type is different
     // Ex: This function below has same name with sayHello() function above
-    fun sayHello(firstNameParam: String, lastNameParam: String) {
-        println("Hello $firstNameParam $lastNameParam, my name is $firstName")
+    fun sayHello(firstName: String, lastName: String) {
+        println("Hello $firstName $lastName, my name is ${this.firstName}")
     }
 }
