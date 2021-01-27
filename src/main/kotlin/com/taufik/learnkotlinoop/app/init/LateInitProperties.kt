be516ddaf72a5219a946354d8309eeb9968b5b71
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-01-27 09:26:18 +0700

    Adding lateinit properties.

---
+++ src/main/kotlin/com/taufik/learnkotlinoop/app/init/LateInitProperties.kt
@@ -0,0 +1,16 @@
+package com.taufik.learnkotlinoop.app.init
+
+import com.taufik.learnkotlinoop.classes.Television
+
+fun main() {
+
+    val television = Television()
+//    println(television.brand) // Error. Because we don't initialize properties at first
+
+    // We can initialize properties either from function or properties in main function or somewhere.
+    television.initTelevision("Samsung")
+    println(television.brand) // Success. We have initialize properties at function.
+
+    television.brand = "GoldStar"
+    println(television.brand) // Success. We have initialize properties at main function.
+}
---
+++ src/main/kotlin/com/taufik/learnkotlinoop/classes/Television.kt
@@ -0,0 +1,15 @@
+package com.taufik.learnkotlinoop.classes
+
+class Television {
+
+    // By default, we must initialize properties. Either the value from constructor/parameter or we input by ourself.
+    // If we don't initialize at first, Kotlin automatically will give an error.
+    // But we can initialize properties later by using lateinit keyword.
+    // The "lateinit" keyword only used for var, mutable data. While we use val, it will give an error.
+    lateinit var brand: String
+
+    // We initialize properties in somewhere, ex: in this function.
+    fun initTelevision(brand: String) {
+        this.brand = brand
+    }
+}
