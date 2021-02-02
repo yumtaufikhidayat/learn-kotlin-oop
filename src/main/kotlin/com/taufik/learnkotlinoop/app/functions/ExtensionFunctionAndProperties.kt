705bc7f02d6e1fa8958e47648d1f0a53db1a9693
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-02-03 06:34:32 +0700

    Adding extension properties.

--- src/main/kotlin/com/taufik/learnkotlinoop/app/functions/ExtensionFunctionAndProperties.kt
+++ src/main/kotlin/com/taufik/learnkotlinoop/app/functions/ExtensionFunctionAndProperties.kt
@@ -2,9 +2,11 @@ package com.taufik.learnkotlinoop.app.functions
 
 import com.taufik.learnkotlinoop.classes.Student
 import com.taufik.learnkotlinoop.classes.sayHello
+import com.taufik.learnkotlinoop.classes.upperName
 
 fun main() {
 
     val student = Student("Eko", 12)
     student.sayHello("Taufik") // We can access extension function by using object
+    println(student.upperName) // We can access extension properties by using object
 }
--- src/main/kotlin/com/taufik/learnkotlinoop/classes/Student.kt
+++ src/main/kotlin/com/taufik/learnkotlinoop/classes/Student.kt
@@ -5,7 +5,7 @@ class Student(val name: String, val age: Int) {
 
 // This called extension function.
 // We don't make any function in class.
-// If we make variable parameter in private or protected, we can't accessed from outside.
+// If we make properties parameter in private or protected, we can't access from outside.
 // By default, extension function is not nullable.
 // We can make extension function nullable by adding ? (question mark).
 fun Student?.sayHello(name: String) {
@@ -13,3 +13,8 @@ fun Student?.sayHello(name: String) {
         println("Hello $name, my name is ${this.name} and my age is ${this.age}")
     }
 }
+
+// This called extension properties.
+// To make extension properties, we can make properties by using getter and/or setter.
+val Student.upperName: String
+    get() = this.name.toUpperCase()
