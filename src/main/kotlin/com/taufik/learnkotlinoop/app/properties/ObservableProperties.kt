8f0d4c6ded830e033fd51f266a4db09d5b90f0af
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-02-16 06:41:39 +0700

    Adding observable properties.

---
+++ src/main/kotlin/com/taufik/learnkotlinoop/app/properties/ObservableProperties.kt
@@ -0,0 +1,11 @@
+package com.taufik.learnkotlinoop.app.properties
+
+import com.taufik.learnkotlinoop.classes.Account
+
+fun main() {
+
+    val account = Account()
+    account.description = "Contoh"
+    account.description = "Diubah"
+    account.description = "Hello world"
+}
--- src/main/kotlin/com/taufik/learnkotlinoop/classes/Account.kt
+++ src/main/kotlin/com/taufik/learnkotlinoop/classes/Account.kt
@@ -1,12 +1,21 @@
 package com.taufik.learnkotlinoop.classes
 
+import kotlin.properties.Delegates
+
 // This class using lazy properties.
 // Lazy is a standard library which has been provided, so that new properties will be initialize when it accessed.
-class Account {
+class Account(description: String = "") {
 
     // This properties return String type, so it must initialize String as return value
     val name: String by lazy {
         println("Name is called")
         "Taufik"
+    }
+
+    // One of standard library that provided by Kotlin for delegation in properties is observable properties.
+    // By using observable properties, we knew value before and value after when changed.
+    var description: String by Delegates.observable(description) {
+        property, oldValue, newValue ->
+            println("${property.name} changed from $oldValue to $newValue")
     }
 }
