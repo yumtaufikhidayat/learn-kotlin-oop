6b0a0f32f2688f9423eb6076845430273d6ef472
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-02-16 07:33:42 +0700

    Adding destructuring declaration lambda parameter.

--- src/main/kotlin/com/taufik/learnkotlinoop/app/declaration/DestructuringDeclaration.kt
+++ src/main/kotlin/com/taufik/learnkotlinoop/app/declaration/DestructuringDeclaration.kt
@@ -1,6 +1,7 @@
 package com.taufik.learnkotlinoop.app.declaration
 
 import com.taufik.learnkotlinoop.classes.Game
+import com.taufik.learnkotlinoop.classes.Login
 import com.taufik.learnkotlinoop.classes.minMax
 
 fun main() {
@@ -18,4 +19,19 @@ fun main() {
     val (min, max) = minMax(100, 200)
     println(min)
     println(max)
+
+    // Use underscore for unused variable
+    val (mins, _) = minMax(10, 200)
+    println(mins)
+//    println(max)
+
+    // Destructuring lambda parameter
+    val login = Login("taufik", "rahasia")
+    login(login){(username, password) ->
+        username == "taufik" && password == "rahasia"
+    }
+}
+
+fun login(login: Login, callback: (Login) -> Boolean): Boolean {
+    return callback(login)
 }
---
+++ src/main/kotlin/com/taufik/learnkotlinoop/classes/Login.kt
@@ -0,0 +1,6 @@
+package com.taufik.learnkotlinoop.classes
+
+data class Login (
+    val username: String,
+    val password: String
+)
