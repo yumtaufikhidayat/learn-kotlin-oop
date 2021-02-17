8a58851d2784edc491082b0ca50992d7fe5cca35
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-02-17 07:29:55 +0700

    Adding exception.

---
+++ src/main/kotlin/com/taufik/learnkotlinoop/app/exception/Exception.kt
@@ -0,0 +1,32 @@
+package com.taufik.learnkotlinoop.app.exception
+
+import com.taufik.learnkotlinoop.exception.ValidationException
+import java.lang.NullPointerException
+
+fun main() {
+
+    // Using try catch exception, multiple catch, and finally
+    // Try catch exception means program will run first, but if any errors, program will throw to catch block automatically.
+    // Multiple catch means we want to know which error specifically appeared.
+    // Finally means either program will run smoothly or not, it will always be executed.
+    try {
+        validateAndSayHello("Taufik")
+        validateAndSayHello("")
+    } catch (error: ValidationException) {
+        println("Error with message ${error.message}")
+    } catch (error: NullPointerException) {
+        println("Error NPE ${error.message}")
+    } finally {
+        println("Finally will always be executed")
+    }
+}
+
+fun validateAndSayHello(name: String) {
+
+    // Making exception
+    if (name.isBlank()) {
+        throw ValidationException("Name is blank")
+    } else {
+        println("Hello $name")
+    }
+}
---
+++ src/main/kotlin/com/taufik/learnkotlinoop/exception/ValidationException.kt
@@ -0,0 +1,8 @@
+package com.taufik.learnkotlinoop.exception
+
+// When we created programs, we will facing error
+// There are so many error, either it is presumed like validation error or not presumed like network error,
+// connect to database error, and many more.
+// Error in Kotlin is represented by Exception, which all error data types will inherit from Throwable class.
+// To make error in Kotlin is so easy, we can use "throw" keyword followed by its error object.
+class ValidationException(message: String) : Throwable(message)
