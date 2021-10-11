d536879ff455972568bdd3d6d1a2253c4e837f87
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-10-11 16:32:22 +0700

    Updating singleton object materials.

--- src/main/kotlin/com/taufik/learnkotlinoop/object/Application.kt
+++ src/main/kotlin/com/taufik/learnkotlinoop/object/Application.kt
@@ -1,13 +1,14 @@
 package com.taufik.learnkotlinoop.`object`
 
 // This is inner object.
-// Inner object is different with inner class, singleton object can't accessed properties or function in outer class.
+// Inner object is different with inner class, singleton object can't access properties or function in outer class.
 class Application(val name: String) {
 
     object Utilities {
         var name = "Utilities"
 
         fun toUpper(value: String): String {
+            println("My name is $name")
             return value.toUpperCase()
         }
     }
--- src/main/kotlin/com/taufik/learnkotlinoop/object/Utilities.kt
+++ src/main/kotlin/com/taufik/learnkotlinoop/object/Utilities.kt
@@ -1,8 +1,8 @@
 package com.taufik.learnkotlinoop.`object`
 
-// Singleton object is object which is create for once.
+// Singleton object is object which is created for once.
 // To make singleton object in Kotlin as same as we make class.
-// Singleton object is similar with class, it can extends or interface. But it doesn't have constructor.
+// Singleton object is similar with class, it can extend or interface. But it doesn't have constructor.
 // To use it, just add object keyword.
 object Utilities {
     var name = "Utilities"
