4cbeccf5f16eca16dd94b8ae6c7bd5370b533e93
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-10-11 17:10:21 +0700

    Updating inline class materials.

--- src/main/kotlin/com/taufik/learnkotlinoop/classes/Token.kt
+++ src/main/kotlin/com/taufik/learnkotlinoop/classes/Token.kt
@@ -1,10 +1,10 @@
 package com.taufik.learnkotlinoop.classes
 
-// Sometimes, we need to make a class with one properties.
+// Sometimes, we need to make a class with one property.
 // Too many classes need more memory to run.
 // Kotlin has solved this issue by using inline class.
 // Inline class is a class that must have exactly one primary constructor parameter.
-// Just add "inline" keyword before class.
+// Just add "inline" keyword before class name.
 inline class Token(val value: String) {
     fun toUpper(): String = value.toUpperCase()
 }
