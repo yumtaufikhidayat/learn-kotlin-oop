e56777c601113c9f5c3e2c542215d24ed8594514
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-10-14 17:02:36 +0700

    Updating exception materials.

--- src/main/kotlin/com/taufik/learnkotlinoop/app/exception/Exception.kt
+++ src/main/kotlin/com/taufik/learnkotlinoop/app/exception/Exception.kt
@@ -8,10 +8,11 @@ fun main() {
     // Using try catch exception, multiple catch, and finally
     // Try catch exception means program will run first, but if any errors, program will throw to catch block automatically.
     // Multiple catch means we want to know which error specifically appeared.
-    // Finally means either program will run smoothly or not, it will always be executed.
+    // Finally, means either program will run smoothly or not, it will always be executed.
     try {
         validateAndSayHello("Taufik")
         validateAndSayHello("")
+        validateAndSayHelloNull(null)
     } catch (error: ValidationException) {
         println("Error with message ${error.message}")
     } catch (error: NullPointerException) {
@@ -28,5 +29,17 @@ fun validateAndSayHello(name: String) {
         throw ValidationException("Name is blank")
     } else {
         println("Hello $name")
+    }
+}
+
+fun validateAndSayHelloNull(name: String?) {
+    if (name != null) {
+        if (name.isBlank()) {
+            throw ValidationException("Name is blank")
+        } else {
+            println("Hello $name")
+        }
+    } else {
+        throw NullPointerException("Name is null")
     }
 }
--- src/main/kotlin/com/taufik/learnkotlinoop/exception/ValidationException.kt
+++ src/main/kotlin/com/taufik/learnkotlinoop/exception/ValidationException.kt
@@ -1,6 +1,6 @@
 package com.taufik.learnkotlinoop.exception
 
-// When we created programs, we will facing error
+// When we created programs, we will face error
 // There are so many error, either it is presumed like validation error or not presumed like network error,
 // connect to database error, and many more.
 // Error in Kotlin is represented by Exception, which all error data types will inherit from Throwable class.
